7285a1d87d17652e555cb608eb2d6b076b8b561d
Karl Louise Rito <karl.rito@switchconnect.com.au>
2022-08-13 14:09:02 +0800

    Project - transition to Vue.js CLI - Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+
+const routes = [
+  { path: "/", name: "home", component: Home },
+  { path: "/about", name: "about", component: About },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
