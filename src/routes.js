9d85c108c5c52b403971809c544c8ad1201470dc
Karl Louise Rito <karl.rito@switchconnect.com.au>
2022-08-14 15:09:46 +0800

    Added 404 not found page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import Home from "./views/Home/home";
 import About from "./views/About/about";
 import Project from "./views/Project/project";
 import Contact from "./views/Contact/contact";
+import NotFound from "./views/NotFound/not_found";
 
 const routes = [
   { path: "/", name: "home", component: Home },
   { path: "/about", name: "about", component: About },
   { path: "/projects", name: "projects", component: Project },
   { path: "/contacts", name: "contact", component: Contact },
+  { path: "/:catchError(.*)", name: "not_found", component: NotFound },
 ];
 
 const router = createRouter({
